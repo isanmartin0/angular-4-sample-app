164fc7deda63df915497a745d6c2967a140fa6dc
isanmartin <isanmartin@keedio.com>
2018-08-26 13:41:13 +0200

    Added sonar-project.properties

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -23,22 +23,14 @@ export class CitiesComponent implements OnInit {
     console.log("City Component Init");;
   }
 
-  function prueba() {
-    i = a += 2, a + b;
 
-    for (var _i = 0; _i < 3; _i++) {
-        var theCity = cityList[_i];
-        console.log(theCity);
-    }
-  }
-
-  function getLength(a: string, b: string, c: string) {
+  getLength(a: string, b: string, c: string) {
     const strings = [];  // Noncompliant
     strings.push(a);
     strings.push(b);
     strings.push(c);
 
-    return a.length + b.length + c.length;
+    return a.length + b.length + c.length;;
   }
 
 }
